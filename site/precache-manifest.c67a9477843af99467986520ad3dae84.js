e9fc755411ac564d29346b84667898d1dd02d609
M3 <006647@spitech.com>
2019-06-27 21:39:21 +0530

    site update

---
+++ site/precache-manifest.c67a9477843af99467986520ad3dae84.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5a0ba0128891d5f6cff3d0582edce2",
+    "url": "./site/index.html"
+  },
+  {
+    "revision": "af356f4e0dbefd535e3e",
+    "url": "./site/static/css/2.4eee7567.chunk.css"
+  },
+  {
+    "revision": "4f44c1f90b9552a9871c",
+    "url": "./site/static/css/main.39dd282d.chunk.css"
+  },
+  {
+    "revision": "af356f4e0dbefd535e3e",
+    "url": "./site/static/js/2.481db9f7.chunk.js"
+  },
+  {
+    "revision": "4f44c1f90b9552a9871c",
+    "url": "./site/static/js/main.0917076c.chunk.js"
+  },
+  {
+    "revision": "75cfed11a2ad12f1dae3",
+    "url": "./site/static/js/runtime~main.cc884d34.js"
+  },
+  {
+    "revision": "0fb3d867ba90d41edcbe3e87f8c5e7b4",
+    "url": "./site/static/media/001.0fb3d867.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./site/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./site/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./site/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./site/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./site/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- site/service-worker.js
+++ site/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./site/precache-manifest.4650ecc7c65a2a7711602060a9d7cd4d.js"
+  "./site/precache-manifest.c67a9477843af99467986520ad3dae84.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./site/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
